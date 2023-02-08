304c5de87c1643701c19e24b6d511a37c04c58d2
Charlie Wei <charlie860716@gmail.com>
2023-02-09 01:53:18 +0800

    add index.html

--- bot/urls.py
+++ bot/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path('callback', views.callback)
+    path('callback', views.callback),
+    path('index', views.index)
 ]
--- bot/views.py
+++ bot/views.py
@@ -20,6 +20,9 @@ from urllib import parse
  
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+def index(request):
+    return render(request, "index.html")
  
  
 @csrf_exempt
